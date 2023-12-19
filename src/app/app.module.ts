33c23e4a4f70cb54067c4ba52d4cda4c66379257
Jorge Argudo <jorge_ruvi@hotmail.es>
2023-12-19 10:47:37 +0100

    Updated styles, routing and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { CustomDropdownComponent } from './components/custom-dropdown/custom-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CustomDropdownComponent
   ],
   imports: [
     BrowserModule,
